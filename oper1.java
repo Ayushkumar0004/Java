aaeedae623095060f548aa7b553946504fff7632
ayush_0001 <ayushk0004@gmail.com>
2024-02-05 11:38:13 +0530

    oper1 (greatest number)

---
+++ oper1.java
@@ -0,0 +1,16 @@
+public class oper1 
+{
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 7;
+        int c = 3;
+        int max = (a > b) ? ((a > c) ? a : c) : ((b > c) ? b : c);
+        if (max == a) {
+            System.out.println("a is the greatest number: " + a);
+        } else if (max == b) {
+            System.out.println("b is the greatest number: " + b);
+        } else {
+            System.out.println("c is the greatest number: " + c);
+        }
+    }
+}
