1284746ff9ceb07bb0aa6140d217c868a4b7f553
ayush_0001 <ayushk0004@gmail.com>
2024-02-05 11:39:57 +0530

    Oper4

---
+++ Oper4.java
@@ -0,0 +1,25 @@
+public class Oper4 {
+    public static void main(String[]  args)
+    {
+        for(int i =0; i < 6; i++)
+        {
+            switch(i)
+            {
+                case 0:
+                System.out.println("i is zero.");
+                case 1:
+                System.out.println("i is one.");
+                case 2:
+                System.out.println("i is two.");
+                case 3:
+                System.out.println("i is three.");
+                case 4:
+                System.out.println("i is four.");
+                case 5:
+                System.out.println("i is five.");
+                case 6:
+                System.out.println("i is six.");
+            }
+        }
+    }
+}
