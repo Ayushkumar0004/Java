31dbb44d4e6b7e6d39e59f0e5176734c59efddab
ayush_0001 <ayushk0004@gmail.com>
2024-02-07 09:55:43 +0530

    intro for each loop in java

---
+++ loop1.java
@@ -0,0 +1,12 @@
+public class loop1 {
+    public static void main(String[] args) {
+        int num[] = {1, 2, 3, 4, 5, 6, 7, 8};
+        for (int i : num) {
+            if (i % 2 == 0) {
+                System.out.println("The even value is : " + i);
+            } else {
+                System.out.println("The odd value is : " + i);
+            }
+        }
+    }
+}
