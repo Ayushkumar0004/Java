6442c73780f30b9291d740dd7d38bd3eeed5d67b
ayush_0001 <ayushk0004@gmail.com>
2024-02-28 10:11:16 +0530

    Constructor java

--- Constructor/Intro.java
+++ Constructor/Intro.java
@@ -1,28 +1,28 @@
-import Inheritance.Intro;
+// import Inheritance;
 
-class Intro {
-    int num1, num2, num3;
+// class Intro {
+//     int num1, num2, num3;
 
-    Intro(int x, int y, int z) {
-        num1 = x;
-        num2 = y;
-        num3 = z;
-    }
+//     Intro(int x, int y, int z) {
+//         num1 = x;
+//         num2 = y;
+//         num3 = z;
+//     }
 
-    void checkGreatest() {
-        if (num1 > num2 && num1 > num3) {
-            System.out.println(num1 + " is the greatest.");
-        } else if (num2 > num1 && num2 > num3) {
-            System.out.println(num2 + " is the greatest.");
-        } else {
-            System.out.println(num3 + " is the greatest.");
-        }
-    }
-}
+//     void checkGreatest() {
+//         if (num1 > num2 && num1 > num3) {
+//             System.out.println(num1 + " is the greatest.");
+//         } else if (num2 > num1 && num2 > num3) {
+//             System.out.println(num2 + " is the greatest.");
+//         } else {
+//             System.out.println(num3 + " is the greatest.");
+//         }
+//     }
+// }
 
-public class Main {
-    public static void main(String[] args) {
-        Intro checker = new Intro(50, 30, 70);
-        checker.checkGreatest();
-    }
-}
+// public class Main {
+//     public static void main(String[] args) {
+//         Intro checker = new Intro(50, 30, 70);
+//         checker.checkGreatest();
+//     }
+// }
