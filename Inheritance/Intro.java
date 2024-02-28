27a294537d64c28957b6fe5606303441382386c7
ayush_0001 <ayushk0004@gmail.com>
2024-02-28 10:09:56 +0530

    Inheritance java

--- Inheritance/Intro.java
+++ Inheritance/Intro.java
@@ -1,22 +1,22 @@
-package Inheritance;
-class A {
-    A() {
-        System.out.println("This is Class A");
-    }
-}
-class B extends A {
-    void fun() {
-        System.out.println("This is Class B");
-    }
-}
-class C extends B {
-    C() {
-        System.out.println("This is Class C");
-    }
-}
-public class Intro {
-    public static void main(String[] args) {
-        C o1 = new C();
-        o1.fun();
-    }
-}
+// package Inheritance;
+// class A {
+//     A() {
+//         System.out.println("This is Class A");
+//     }
+// }
+// class B extends A {
+//     void fun() {
+//         System.out.println("This is Class B");
+//     }
+// }
+// class C extends B {
+//     C() {
+//         System.out.println("This is Class C");
+//     }
+// }
+// public class Intro {
+//     public static void main(String[] args) {
+//         C o1 = new C();
+//         o1.fun();
+//     }
+// }
