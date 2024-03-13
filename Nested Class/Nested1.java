8285c0fb7010a383625d04fe51cdf486ef507d2f
ayush_0001 <ayushk0004@gmail.com>
2024-03-13 10:10:56 +0530

    static nested class java

---
+++ Nested Class/Nested1.java
@@ -0,0 +1,17 @@
+// package Nested Class;
+
+class A {
+    void fun() {}
+
+    static class B {
+        void fun1() {
+            System.out.println("class A");
+        }
+    }
+}
+public class Nested1 {
+    public static void main(String[] args) {
+        A.B ob1 = new A.B();
+        ob1.fun1();
+    }
+}
