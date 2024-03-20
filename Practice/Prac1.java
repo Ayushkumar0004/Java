20e4ca9bb5670232051ba15eb50d1f2c0db6e953
ayush_0001 <ayushk0004@gmail.com>
2024-03-20 18:26:30 +0530

    acc and dec of speed using inheritance

---
+++ Practice/Prac1.java
@@ -0,0 +1,30 @@
+// // package Practice;
+// // Create a class Vehicle with a field speed and methods accelerate() and brake() to increase and decrease the speed of the vehicle, respectively.
+// public class Prac1 {
+//     private int speed;
+
+//     public int getSpeed() {
+//         return speed;
+//     }
+
+//     public void setSpeed(int s) {
+//         this.speed = s;
+//     }
+
+//     public void accelerate(int inc) {
+//         speed += inc;
+//     }
+
+//     public void brake(int dec) {
+//         speed -= dec;
+//     }
+//     public static void main(String[] args) {
+//         Prac1 prac = new Prac1();
+//         prac.setSpeed(40);
+//         System.out.println("Initial speed: " + prac.getSpeed());
+//         prac.accelerate(80);
+//         System.out.println("Current speed after acceleration: " + prac.getSpeed());
+//         prac.brake(20);
+//         System.out.println("Current speed after braking: " + prac.getSpeed());
+//     }
+// }
