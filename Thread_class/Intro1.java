6eadde35fe2abe3196cd92f1ae5ae0c43fa7a4c2
ayush_0001 <ayushk0004@gmail.com>
2024-03-20 10:49:10 +0530

    Intro of threading in java

---
+++ Thread_class/Intro1.java
@@ -0,0 +1,36 @@
+// package Thread_class;
+// class MyThread extends Thread {
+//     public void run() {
+//         for(int i = 0; i < 10; i++) {
+//             System.out.println("Hello class");
+//             try {
+//                 Thread.sleep(100);
+//             } catch(Exception e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
+
+// class Verma extends Thread {
+//     public void run() {
+//         for(int i = 0; i < 10; i++) {
+//             System.out.println("Hello AYUSH");
+//             try {
+//                 Thread.sleep(100);
+//             } catch(Exception e) {
+//                 e.printStackTrace();
+//             }
+//         }
+//     }
+// }
+
+// public class Intro1 {
+//     public static void main(String[] args) {
+//         MyThread myThread = new MyThread();
+//         Verma verma = new Verma();
+
+//         myThread.start();
+//         verma.start();
+//     }
+// }
