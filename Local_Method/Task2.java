5158c144678e876039c4f4b4dbc8ecbda954d7e8
ayush_0001 <ayushk0004@gmail.com>
2024-03-22 15:04:09 +0530

    update the date using local date

---
+++ Local_Method/Task2.java
@@ -0,0 +1,12 @@
+// import java.time.LocalDate;
+
+// public class Task2 {
+//     public static void main(String[] args) {
+
+//         LocalDate date = LocalDate.of(2007, 1, 3);
+//         System.out.println("Original date: " + date);
+
+//         LocalDate updatedDate = date.withDayOfYear(90);
+//         System.out.println("Updated date: " + updatedDate);
+//     }
+// }
