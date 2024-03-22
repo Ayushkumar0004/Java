7a55543cdada7c4a3061a397e27d86237c6ded27
ayush_0001 <ayushk0004@gmail.com>
2024-03-22 15:05:37 +0530

    add the time using local time

---
+++ Local_Method/Task4.java
@@ -0,0 +1,9 @@
+// import java.time.LocalTime;
+// public class Task4 {
+//     public static void main(String[] args) {
+ 
+//         LocalTime time = LocalTime.of(14, 30);
+//         LocalTime updatedTime = time.plusHours(2).plusMinutes(15).plusSeconds(30);
+//         System.out.println("Updated time: " + updatedTime);
+//     }
+// }
