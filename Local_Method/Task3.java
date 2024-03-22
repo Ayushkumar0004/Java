02e56565848f594175acf670c2bcbd5108bda858
ayush_0001 <ayushk0004@gmail.com>
2024-03-22 15:04:59 +0530

    update the month using local date

---
+++ Local_Method/Task3.java
@@ -0,0 +1,11 @@
+// import java.time.LocalDate;
+
+// public class Task3 {
+//     public static void main(String[] args) {
+//         LocalDate date = LocalDate.of(2007, 1, 3);
+//         System.out.println("Original date: " + date);
+
+//         LocalDate updatedDate = date.withMonth(12);
+//         System.out.println("Updated date: " + updatedDate);
+//     }
+// }
