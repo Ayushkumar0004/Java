a9b437e19c318e3e4d36cf085657a1badb7d5d9f
ayush_0001 <ayushk0004@gmail.com>
2024-03-27 10:17:47 +0530

    Intro of Exception_handling

---
+++ Exception_handling/Intro1.java
@@ -0,0 +1,17 @@
+// // package Exception_handling;
+// public class Intro1 
+// {
+//     public static void main(String[] args) {
+//         System.out.println("Hello class");
+//         int a = 10, b = 0, c;
+//         try{
+//         c = a / b;
+//         }
+//         catch(Exception e)
+//         {
+//             System.out.println("Error in the statement");
+//         }
+//         System.out.println(b);
+//         System.out.println("Bye class");
+//     }
+// }
