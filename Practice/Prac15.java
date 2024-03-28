5e87818d04cd652e4fdb66776a870888348f953d
ayush_0001 <ayushk0004@gmail.com>
2024-03-28 21:48:20 +0530

    perfect square using lambda expression

---
+++ Practice/Prac15.java
@@ -0,0 +1,23 @@
+// Determine if a given number is a perfect square using a lambda expression.
+import java.util.*;
+
+@FunctionalInterface
+interface PerfectSquare {
+    boolean isPerfectSquare(int n);
+}
+
+public class Prac15 {
+    public static void main(String[] args) {
+        int num = 20;
+        PerfectSquare ps = (n) -> {
+            double sqrt = Math.sqrt(n);
+            return sqrt == Math.floor(sqrt);
+        };
+
+        if (ps.isPerfectSquare(num)) {
+            System.out.println(num + " is a perfect square ");
+        } else {
+            System.out.println(num + " is not a perfect square");
+        }
+    }
+}
