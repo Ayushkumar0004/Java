d1ffddfc82d941ebcee45b62b8e3c5078bbfe33c
ayush_0001 <ayushk0004@gmail.com>
2024-04-10 10:21:06 +0530

    find longest word from the file in File_Handling

--- File_Handling/Task2.java
+++ File_Handling/Task2.java
@@ -1,30 +1,21 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+// import java.io.File;
+// import java.io.FileNotFoundException;
+// import java.util.Scanner;
 
-public class Task2 {
-    public static void main(String[] args) {
-        try {
-            File file = new File("D");
-            Scanner dataReader;
-            dataReader = new Scanner(file);
-            while (dataReader.hasNextLine()) {
-                String fileData = dataReader.nextLine();
-                System.out.println(fileData);
-            }
-            dataReader.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("An unexpected error occurred.");
-            e.printStackTrace();
-        }
-    }
-}
-File f1 = new File("D");
-if(f1.delete())
-{
-    System.out.println(f1.getName() + " file is created successfully");
-}
-else
-{
-    System.out.println("Unexpected error found inn deletion of file");
-}
+// public class Task2 {
+//     public static void main(String[] args) {
+//         try {
+//             File file = new File("D");
+//             Scanner dataReader;
+//             dataReader = new Scanner(file);
+//             while (dataReader.hasNextLine()) {
+//                 String fileData = dataReader.nextLine();
+//                 System.out.println(fileData);
+//             }
+//             dataReader.close();
+//         } catch (FileNotFoundException e) {
+//             System.out.println("An unexpected error occurred.");
+//             e.printStackTrace();
+//         }
+//     }
+// }
