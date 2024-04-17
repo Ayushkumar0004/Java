35110862ac0999219b2a91ab1f1c6b7acd246b28
ayush_0001 <ayushk0004@gmail.com>
2024-04-17 10:27:39 +0530

    Intro of generic class

---
+++ Generic/Intro.java
@@ -0,0 +1,18 @@
+// // package Generic;
+// class A<T>
+// {
+//     T a;
+//     void fun(T b)
+//     {
+//         a = b;
+//         System.out.println(a);
+//     }
+// }
+// public class Intro {
+//     public static void main(String[] args) {
+//         A<Integer> o1 = new A<Integer>();
+//         A<String> o2 = new A<String>();
+//         o1.fun(20);
+//         o2.fun("Hello");
+//     }
+// }
