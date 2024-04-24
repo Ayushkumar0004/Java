68c575009a1d7340264190caea4a3985e644e075
ayush_0001 <ayushk0004@gmail.com>
2024-04-24 10:51:08 +0530

    Method interface in Generic

---
+++ Generic/Method1.java
@@ -0,0 +1,16 @@
+interface A<T> {
+    void fun(T a);
+}
+
+public class Method1<T> implements A<T> {
+    public void fun(T a) {
+        System.out.println(a);
+    }
+
+    public static void main(String[] args) {
+        Method1<Integer> o1 = new Method1<>();
+        o1.fun(10);
+        Method1<String> o2 = new Method1<>();
+        o2.fun("AYUSH");
+    }
+}
