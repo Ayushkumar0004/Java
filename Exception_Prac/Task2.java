df3b48f2cfbe43d06fbb71495dedddc575d37035
ayush_0001 <ayushk0004@gmail.com>
2024-04-24 23:00:33 +0530

    Exception_Practise 2

---
+++ Exception_Prac/Task2.java
@@ -0,0 +1,21 @@
+// // package Exception_Prac;/
+// // Write a Java program that asks the user to enter an integer, but if the user enters a non-integer value, catch and handle the InputMismatchException
+
+// import java.util.InputMismatchException;
+// import java.util.Scanner;
+
+// public class Task2 {
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("Enter the number");
+//         try
+//         {
+//             int num = sc.nextInt();
+//             System.out.println("The value is Integer");
+//         }
+//         catch(InputMismatchException e)
+//         {
+//             System.out.println("The input is non integer value");
+//         }
+//     }
+// }
