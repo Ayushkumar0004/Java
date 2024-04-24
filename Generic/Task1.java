c845e0344ef690ed6dc944ec4808272e69333e18
ayush_0001 <ayushk0004@gmail.com>
2024-04-24 10:06:52 +0530

    Generic class

---
+++ Generic/Task1.java
@@ -0,0 +1,24 @@
+class A<M>
+{
+    M a;
+    void fun(M b)
+    {
+        a = b;
+        System.out.println(a);
+    }
+}
+public class Task1 
+{
+    public static void main(String[] args) 
+    {
+        A<Integer> o1= new A<>();
+        o1.fun(10);
+        o1.fun(100);
+        A<String> o2 = new A<>();
+        o2.fun("Hello");
+        A<Float> o3 = new A<>();
+        o3.fun(105.4F);
+        A<Double> o4 = new A<>();
+        o4.fun(105.4);
+    }
+}
