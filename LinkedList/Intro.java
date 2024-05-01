90e4479ee30326ba36183317b79eb2884b7fe3a6
ayush_0001 <ayushk0004@gmail.com>
2024-05-01 10:24:54 +0530

    LinkedList

--- LinkedList/Intro.java
+++ LinkedList/Intro.java
@@ -1,12 +1,12 @@
-// package LinkedList;
+// // package LinkedList;
 
-import java.util.LinkedList;
+// import java.util.LinkedList;
 
-public class Intro {
-    public static void main(String[] args) {
-        LinkedList<Integer> o1 = new LinkedList<>();
-        o1.add(200);
-        o1.addFirst(null);
-        System.out.println(o1);
-    }
-}
+// public class Intro {
+//     public static void main(String[] args) {
+//         LinkedList<Integer> o1 = new LinkedList<>();
+//         o1.add(200);
+//         o1.addFirst(null);
+//         System.out.println(o1);
+//     }
+// }
--- LinkedList/Task1.java
+++ LinkedList/Task1.java
@@ -1,28 +1,28 @@
-import java.util.*;
-public class Task1 {
-    public static void main(String[] args) {
-        LinkedList<Integer> o1 = new LinkedList<>();
-        Scanner sc = new Scanner(System.in);
+// import java.util.*;
+// public class Task1 {
+//     public static void main(String[] args) {
+//         LinkedList<Integer> o1 = new LinkedList<>();
+//         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Enter the number of elements in the linked list: ");
-        int n = sc.nextInt();
+//         System.out.println("Enter the number of elements in the linked list: ");
+//         int n = sc.nextInt();
         
-        System.out.println("Enter the elements: ");
-        for (int i = 0; i < n; i++) {
-            int element = sc.nextInt();
-            o1.add(element);
-        }
+//         System.out.println("Enter the elements: ");
+//         for (int i = 0; i < n; i++) {
+//             int element = sc.nextInt();
+//             o1.add(element);
+//         }
 
-        System.out.println("Original List: " + o1);
-        int middleIndex = n / 2;
-        if (n % 2 == 0) {
-            o1.remove(middleIndex - 1);
-        } 
-        else 
-        {
-            o1.remove(middleIndex);
-        }
-        System.out.println("List after removing middle element: " + o1);
-        sc.close();
-    }
-}
+//         System.out.println("Original List: " + o1);
+//         int middleIndex = n / 2;
+//         if (n % 2 == 0) {
+//             o1.remove(middleIndex - 1);
+//         } 
+//         else 
+//         {
+//             o1.remove(middleIndex);
+//         }
+//         System.out.println("List after removing middle element: " + o1);
+//         sc.close();
+//     }
+// }
