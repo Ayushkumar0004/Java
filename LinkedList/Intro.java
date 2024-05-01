6c246da7fd413d3526f7a28756bbb5faba7d5a7e
ayush_0001 <ayushk0004@gmail.com>
2024-05-01 09:48:23 +0530

    Intro to LinkedList

---
+++ LinkedList/Intro.java
@@ -0,0 +1,12 @@
+// package LinkedList;
+
+import java.util.LinkedList;
+
+public class Intro {
+    public static void main(String[] args) {
+        LinkedList<Integer> o1 = new LinkedList<>();
+        o1.add(200);
+        o1.addFirst(null);
+        System.out.println(o1);
+    }
+}
