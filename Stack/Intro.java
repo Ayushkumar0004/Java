c638861b741f03bd7f399a31359ab33c334780a9
ayush_0001 <ayushk0004@gmail.com>
2024-05-01 10:07:36 +0530

    intro to Stack

---
+++ Stack/Intro.java
@@ -0,0 +1,15 @@
+// // package Stack; 
+// import java.util.Stack;
+
+// public class Intro 
+// {
+//     public static void main(String[] args) 
+//     {
+//         Stack<Integer> o1 = new Stack<>();
+//         o1.add(200);
+//         o1.add(20);
+//         o1.push(100);   
+//         o1.pop();
+//         System.out.println(o1.peek()); 
+//     }    
+// }
