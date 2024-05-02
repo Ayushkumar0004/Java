9ba189a05270a8d680be195094b9b2e79c429368
ayush_0001 <ayushk0004@gmail.com>
2024-05-02 15:33:07 +0530

    Priority Queue in java

---
+++ Queue/MyPriorityQueue.java
@@ -0,0 +1,12 @@
+// package Queue;
+import java.util.*;
+
+public class MyPriorityQueue {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> o1 = new PriorityQueue<>();
+        o1.offer(54);
+        o1.offer(25);
+        o1.offer(58);
+        System.out.println(o1);
+    }
+}
