415af8561158d04cd2a76f8d3f5a29709c02f7e5
ayush_0001 <ayushk0004@gmail.com>
2024-05-02 09:46:54 +0530

    search in Queue

---
+++ Queue/QueueSearch.java
@@ -0,0 +1,17 @@
+// // package Queue;
+// import java.util.*;
+
+// public class QueueSearch {
+//     public static void main(String[] args) {
+//         Queue<Integer> o1 = new LinkedList<>();
+//         o1.add(10);
+//         o1.add(20);
+//         o1.add(30);
+//         int elementSearch = 20;
+//         if (o1.contains(elementSearch)) {
+//             System.out.println("Element " + elementSearch + " is present in the queue.");
+//         } else {
+//             System.out.println("Element " + elementSearch + " is not present in the queue.");
+//         }
+//     }
+// }
