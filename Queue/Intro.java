6ca84c5fcd2350984f31b4b159c2555cd2bd2a8e
ayush_0001 <ayushk0004@gmail.com>
2024-05-02 09:31:46 +0530

    implement all the method of queue

---
+++ Queue/Intro.java
@@ -0,0 +1,19 @@
+// import java.util.*;
+// public class Intro {
+//     public static void main(String[] args) {
+//         Queue<Integer> o1 = new LinkedList<>();
+//         o1.add(54);
+//         o1.offer(25);
+//         o1.offer(58);
+//         System.out.println(o1);
+
+//         o1.remove();
+//         o1.poll();
+//         o1.addAll(o1);
+//         boolean containsItself = o1.contains(58);
+//         boolean isEmpty = o1.isEmpty();
+//         System.out.println(o1);
+//         System.out.println("Contains itself: " + containsItself);
+//         System.out.println("Is Empty: " + isEmpty);
+//     }
+// }
