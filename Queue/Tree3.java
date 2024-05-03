f629dc80108c091689f902b5a676e0fa4935c86b
ayush_0001 <ayushk0004@gmail.com>
2024-05-03 10:25:36 +0530

    Sorting using implemets

--- Queue/Tree3.java
+++ Queue/Tree3.java
@@ -1,28 +1,28 @@
-import java.util.TreeSet;
-class Myclass implements Comparable<Myclass> {
-    private int value;
+// import java.util.TreeSet;
+// class Myclass implements Comparable<Myclass> {
+//     private int value;
 
-    public Myclass(int v) {
-        this.value = v;
-    }
+//     public Myclass(int v) {
+//         this.value = v;
+//     }
 
-    public int getValue() {
-        return value;
-    }
-    @Override
-    public int compareTo(Myclass other) {
-        return Integer.compare(this.value, other.value);
-    }
-}
-public class Tree3 {
-    public static void main(String[] args) {
-        TreeSet<Myclass> myTreeSet = new TreeSet<>();
-        myTreeSet.add(new Myclass(5));
-        myTreeSet.add(new Myclass(3));
-        myTreeSet.add(new Myclass(8));
-        System.out.println("Elements in TreeSet: ");
-        for (Myclass o1 : myTreeSet) {
-            System.out.println(o1.getValue());
-        }
-    }
-}
+//     public int getValue() {
+//         return value;
+//     }
+//     @Override
+//     public int compareTo(Myclass other) {
+//         return Integer.compare(this.value, other.value);
+//     }
+// }
+// public class Tree3 {
+//     public static void main(String[] args) {
+//         TreeSet<Myclass> myTreeSet = new TreeSet<>();
+//         myTreeSet.add(new Myclass(5));
+//         myTreeSet.add(new Myclass(3));
+//         myTreeSet.add(new Myclass(8));
+//         System.out.println("Elements in TreeSet: ");
+//         for (Myclass o1 : myTreeSet) {
+//             System.out.println(o1.getValue());
+//         }
+//     }
+// }
